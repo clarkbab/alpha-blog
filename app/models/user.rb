f095490bcda2bdb342f9db18b196a2ac47de7683
Brett Clark <clarkbab.developer@gmail.com>
2016-05-18 12:02:08 +1000

    Added: User model validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,26 @@
 class User < ActiveRecord::Base
-    
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    validates :username,
+        presence: true,
+        length: {
+            minimum: 3,
+            maximum: 25
+        },
+        uniqueness: {
+            case_sensitive: false
+        }
+
+    validates :email,
+        presence: true,
+        length: {
+            maximum: 50
+        },
+        format: {
+            with: VALID_EMAIL_REGEX
+        },
+        uniqueness: {
+            case_sensitive: false
+        }
+
 end
