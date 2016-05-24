f617ab00d8c8970a99f120c55e4f45a546a7c63b
Brett Clark <clarkbab.developer@gmail.com>
2016-05-24 11:09:42 +1000

    Added: Category validation tests and implementation.

---
+++ test/models/category_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class CategoryTest < ActiveSupport::TestCase
+
+    def setup
+        @category = Category.new(name: "sports")
+    end
+
+    test "should be valid" do
+        assert @category.valid?    
+    end
+
+    test "should not accept blank names" do
+        @category.name = ""
+        assert_not @category.valid?
+    end
+
+    test "should not accept names that already exist" do
+        @category.save
+        category2 = Category.new(name: "sports")
+        assert_not category2.valid?
+    end
+
+    test "should not accept names less than three characters long" do
+        @category.name = "aa"
+        assert_not @category.valid?
+    end
+
+    test "should not accept names more than twenty-five characters long" do
+        @category.name = "a" * 26
+        assert_not @category.valid?
+    end
+end
